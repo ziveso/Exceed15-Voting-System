0f273e53a1cb4a537a27132beb15da20aeaf3eb6
winChawakorn <chawakorn.su@ku.th>
2018-07-08 23:05:01 +0700

    Add eiei

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 // import * as firebase from "firebase";
 // import firebaseConfig from './Firebase/Config'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Login , isLogin, Logout } from './Login/User'
+import { Login, isLogin, Logout } from './Login/User'
 
 export class App extends Component {
 
@@ -17,26 +17,26 @@ export class App extends Component {
     }
     componentDidMount() {
         const isLogged = isLogin()
-        if(isLogged) {
+        if (isLogged) {
             this.setState({ user: isLogged, isLogin: true })
         }
     }
 
     handleChange(e) {
-        this.setState({ login:e.target.value })
+        this.setState({ login: e.target.value })
     }
-    
+
     render() {
         return (
             <Router>
                 <div>
                     {/* Navbar */}
-                    { this.state.isLogin? <h1>Logged-in</h1>: <h1>Not Log-in</h1>}
-                    <input value={this.state.login} onChange={ this.handleChange }/>
+                    {this.state.isLogin ? <h1>Logged-in</h1> : <h1>Not Log-in</h1>}
+                    <input value={this.state.login} onChange={this.handleChange} />
                     {/* แยก component มี state เป็นของตัวเอง */}
-                    <button onClick={ () => Login(this.state.login) }>Login</button>
+                    <button onClick={() => Login(this.state.login)}>Login</button>
                     {/* แยก component มี state เป็นของตัวเอง */}
-                    <button onClick={ () => Logout() }>Logout</button>
+                    <button onClick={() => Logout()}>Logout</button>
                     {/* อย่าลืม update state ใน class นี้เพื่อ จะได้เอาไปเปลี่ยน navbar */}
 
 
@@ -47,6 +47,7 @@ export class App extends Component {
                     {/* <Route path='/admin' /> */}
                     {/* USER */}
                     {/* <Route path='/' /> */}
+                    {/*EIEI*/}
                 </div>
             </Router>
         )
