de6e34191dc1dab56db44bdb07899e04d1f4ae9e
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-07-10 01:19:56 +0700

    prepare dashbaord

--- src/Dashboard/Index.js
+++ src/Dashboard/Index.js
@@ -1,7 +1,54 @@
 import React, { Component } from 'react'
 
+const data = [
+  {
+    name: 'SKE GOGO',
+    percent: '80%',
+  },
+  {
+    name: 'SKE GOGO',
+    percent: '80%',
+  },{
+    name: 'SKE GOGO',
+    percent: '80%',
+  },{
+    name: 'SKE GOGO',
+    percent: '80%',
+  },{
+    name: 'SKE GOGO',
+    percent: '80%',
+  },{
+    name: 'SKE GOGO',
+    percent: '80%',
+  },{
+    name: 'SKE GOGO',
+    percent: '80%',
+  },
+]
+
 export class Index extends Component {
   render () {
+    const leaderComponent = data.map((item, index) => {
+      if (index < 1) {
+        // add div gold class
+        // <div className='col-md-12'>
+        //   <img style={{ width: '45%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
+        //   <h1 style={{ color: 'gold' }}>Teamname 90%</h1>
+        // </div>
+      } else if (index < 3) {
+        // add div silver class
+        // <div className='col-md-6'>
+        //   <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
+        //   <h2 style={{ color: 'silver' }}>Teamname 80%</h2>
+        // </div>
+      } else {
+        // add div other class
+        // <div className='col-md-4'>
+        //   <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
+        //   <h3>Teamname 70%</h3>
+        // </div>
+      }
+    })
     return (
       <div className='row text-center'>
         <div className='col-12'>
@@ -13,36 +60,36 @@ export class Index extends Component {
           <div className='row'>
             <div className='container'>
               <img style={{ width: '45%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
-              <h1>90%</h1>
+              <h1 style={{ color: 'gold' }}>Teamname 90%</h1>
             </div>
           </div>
 
           <div className='row' style={{ marginTop: '20px' }}>
             <div className='col-md-6'>
               <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
-              <h1>80%</h1>
+              <h2 style={{ color: 'silver' }}>Teamname 80%</h2>
             </div>
 
             <div className='col-md-6'>
               <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
-              <h1>80%</h1>
+              <h2 style={{ color: 'silver' }}>Teamname 80%</h2>
             </div>
           </div>
 
           <div className='row' style={{ marginTop: '20px' }}>
             <div className='col-md-4'>
               <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
-              <h1>70%</h1>
+              <h3>Teamname 70%</h3>
             </div>
 
             <div className='col-md-4'>
               <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
-              <h1>70%</h1>
+              <h3>Teamname 70%</h3>
             </div>
 
             <div className='col-md-4'>
               <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
-              <h1>70%</h1>
+              <h3>Teamname 70%</h3>
             </div>
           </div>
         </div>
@@ -53,36 +100,36 @@ export class Index extends Component {
           <div className='row'>
             <div className='container'>
               <img style={{ width: '45%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
-              <h1>90%</h1>
+              <h1 style={{ color: 'gold' }}>Teamname 90%</h1>
             </div>
           </div>
 
           <div className='row' style={{ marginTop: '20px' }}>
             <div className='col-md-6'>
               <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
-              <h1>80%</h1>
+              <h2 style={{ color: 'silver' }}>Teamname 80%</h2>
             </div>
 
             <div className='col-md-6'>
               <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
-              <h1>80%</h1>
+              <h2 style={{ color: 'silver' }}>Teamname 80%</h2>
             </div>
           </div>
 
           <div className='row' style={{ marginTop: '20px' }}>
             <div className='col-md-4'>
               <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
-              <h1>70%</h1>
+              <h3>Teamname 70%</h3>
             </div>
 
             <div className='col-md-4'>
               <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
-              <h1>70%</h1>
+              <h3>Teamname 70%</h3>
             </div>
 
             <div className='col-md-4'>
               <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
-              <h1>70%</h1>
+              <h3>Teamname 70%</h3>
             </div>
           </div>
         </div>
