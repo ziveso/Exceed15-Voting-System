af8a03b86f843394e8aa4347a96024e21f0090eb
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-07-10 01:06:50 +0700

    setup dashboard with mockdata

--- src/Dashboard/Index.js
+++ src/Dashboard/Index.js
@@ -3,8 +3,89 @@ import React, { Component } from 'react'
 export class Index extends Component {
   render () {
     return (
-      <div>
-        Exceed 15
+      <div className='row text-center'>
+        <div className='col-12'>
+          <h1>Leaderboard Exceed 15</h1>
+        </div>
+        <div className='col-md-6'>
+          <h3>Software</h3>
+
+          <div className='row'>
+            <div className='container'>
+              <img style={{ width: '45%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
+              <h1>90%</h1>
+            </div>
+          </div>
+
+          <div className='row' style={{ marginTop: '20px' }}>
+            <div className='col-md-6'>
+              <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
+              <h1>80%</h1>
+            </div>
+
+            <div className='col-md-6'>
+              <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
+              <h1>80%</h1>
+            </div>
+          </div>
+
+          <div className='row' style={{ marginTop: '20px' }}>
+            <div className='col-md-4'>
+              <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
+              <h1>70%</h1>
+            </div>
+
+            <div className='col-md-4'>
+              <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
+              <h1>70%</h1>
+            </div>
+
+            <div className='col-md-4'>
+              <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
+              <h1>70%</h1>
+            </div>
+          </div>
+        </div>
+
+        <div className='col-md-6'>
+          <h3>Hardware</h3>
+
+          <div className='row'>
+            <div className='container'>
+              <img style={{ width: '45%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
+              <h1>90%</h1>
+            </div>
+          </div>
+
+          <div className='row' style={{ marginTop: '20px' }}>
+            <div className='col-md-6'>
+              <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
+              <h1>80%</h1>
+            </div>
+
+            <div className='col-md-6'>
+              <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
+              <h1>80%</h1>
+            </div>
+          </div>
+
+          <div className='row' style={{ marginTop: '20px' }}>
+            <div className='col-md-4'>
+              <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
+              <h1>70%</h1>
+            </div>
+
+            <div className='col-md-4'>
+              <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
+              <h1>70%</h1>
+            </div>
+
+            <div className='col-md-4'>
+              <img style={{ width: '50%', height: 'auto' }} src='http://journals.plos.org/plosbiology/article/figure/image?size=medium&id=info:doi/10.1371/image.pbio.v15.i04.g001' alt='group' />
+              <h1>70%</h1>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
