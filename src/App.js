731854bc3131d419ac5d4f5914f949bd7768e77b
Chawakorn Suphepre <chawakorn.su@ku.th>
2018-07-09 09:07:34 +0700

    Login system (#1)
    * done login system
    
    * Remove eiei

--- src/App.js
+++ src/App.js
@@ -1,57 +1,81 @@
 import React, { Component } from 'react'
 // import * as firebase from "firebase";
 // import firebaseConfig from './Firebase/Config'
+import { InputGroup, InputGroupAddon, InputGroupText, Input, Button } from 'reactstrap'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Login, isLogin, Logout } from './Login/User'
+import './App.css'
 
 export class App extends Component {
 
-    constructor() {
-        super()
-        this.state = {
-            user: null,
-            isLogin: false,
-            login: ""
-        }
-        this.handleChange = this.handleChange.bind(this)
+  constructor() {
+    super()
+    this.state = {
+      user: '',
+      isLogin: false,
+      id: ''
     }
-    componentDidMount() {
-        const isLogged = isLogin()
-        if (isLogged) {
-            this.setState({ user: isLogged, isLogin: true })
-        }
-    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  componentDidMount() {
+    const isLogged = isLogin()
+    if (isLogged)
+      this.setState({ user: isLogged, isLogin: true })
+  }
 
-    handleChange(e) {
-        this.setState({ login: e.target.value })
-    }
+  handleChange(e) {
+    this.setState({ id: e.target.value })
+  }
 
-    render() {
-        return (
-            <Router>
-                <div>
-                    {/* Navbar */}
-                    {this.state.isLogin ? <h1>Logged-in</h1> : <h1>Not Log-in</h1>}
-                    <input value={this.state.login} onChange={this.handleChange} />
-                    {/* แยก component มี state เป็นของตัวเอง */}
-                    <button onClick={() => Login(this.state.login)}>Login</button>
-                    {/* แยก component มี state เป็นของตัวเอง */}
-                    <button onClick={() => Logout()}>Logout</button>
-                    {/* อย่าลืม update state ใน class นี้เพื่อ จะได้เอาไปเปลี่ยน navbar */}
+  login() {
+    const id = Login(this.state.id)
+    if (id === -1)
+      console.log('Throw modal')
+    else
+      this.setState({ user: id, isLogin: true })
+  }
 
+  logout() {
+    Logout()
+    this.setState({ user: '', isLogin: false, id: '' })
+  }
 
-
-                    {/* TODO */}
-                    {/* REACT ROUTER */}
-                    {/* ADMIN */}
-                    {/* <Route path='/admin' /> */}
-                    {/* USER */}
-                    {/* <Route path='/' /> */}
-                    {/*EIEI*/}
+  render() {
+    return (
+      <Router>
+        <div>
+          {/* Navbar */}
+          <div className='nav' style={{ padding: '20px' }}>
+            <h2 style={{ marginLeft: '50px' }}>EXCEED 15th</h2>
+            <div style={{ marginLeft: '50px' }}>
+              {this.state.isLogin ?
+                <div style={{ display: 'flex' }}>
+                  <h5 style={{ margin: 'auto', color: 'yellow' }}>{this.state.user}</h5>
+                  <Button color='danger' className="nav-button" onClick={() => this.logout()}>Logout</Button>
                 </div>
-            </Router>
-        )
-    }
+                : <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                  <InputGroup size="sm" style={{ width: '240px', height: '40px', marginBottom: '10px', }}>
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>Student ID</InputGroupText>
+                    </InputGroupAddon>
+                    <Input placeholder="Student ID" value={this.state.id} onChange={this.handleChange} />
+                  </InputGroup>
+                  <Button color='primary' className="nav-button" onClick={() => this.login()}>Login</Button>
+                </div>}
+            </div>
+          </div>
+          <Button onClick={() => console.log(this.state)}>Log state</Button>
+
+          {/* TODO */}
+          {/* REACT ROUTER */}
+          {/* ADMIN */}
+          {/* <Route path='/admin' /> */}
+          {/* USER */}
+          {/* <Route path='/' /> */}
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App
---
+++ src/User/Vote.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Alert, Jumbotron, Button } from 'reactstrap'
+import { isLogin } from '../Login/User'
+
+class Vote extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLogin: isLogin()
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Jumbotron>
+          <h1>Group 1</h1>
+        </Jumbotron>
+      </div>
+    )
+  }
+}
+
+export default Vote
