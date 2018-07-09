dab38bcc8d9eb1dd8d0507a3ae3d93d69c3754cc
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-07-10 00:13:40 +0700

    Fix guy

--- src/App.js
+++ src/App.js
@@ -9,58 +9,58 @@ import './App.css'
 import { Button } from 'reactstrap'
 
 export class App extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			user: '',
-			isLogin: false,
-			id: ''
-		}
-		firebase.initializeApp(firebaseConfig);
-	}
+  constructor (props) {
+    super(props)
+    this.state = {
+      user: '',
+      isLogin: false,
+      id: ''
+    }
+    firebase.initializeApp(firebaseConfig)
+  }
 
-	componentDidMount() {
-		const isLogged = isLoggedIn()
-		if (isLogged)
-			this.setState({ user: isLogged, isLogin: true })
-	}
+  componentDidMount () {
+    const isLogged = isLoggedIn()
+    if (isLogged) this.setState({ user: isLogged, isLogin: true })
+  }
 
-	login() {
-		const id = login(this.state.id)
-		if (id === -1)
-			console.log('Throw modal')
-		else
-			this.setState({ user: id, isLogin: true })
-	}
+  login () {
+    const id = login(this.state.id)
+    if (id === -1) console.log('Throw modal')
+    else this.setState({ user: id, isLogin: true })
+  }
 
-	logout() {
-		logout()
-		this.setState({ user: '', isLogin: false, id: '' })
-	}
+  logout () {
+    logout()
+    this.setState({ user: '', isLogin: false, id: '' })
+  }
 
-	render() {
-		return (
-			<Router>
-				<div>
-					<Button onClick={() => console.log(this.state)}>Log state</Button>
-					<Link to="/admin">หน้าแอดมินจ้า</Link>
-					{/* TODO */}
-					{/* REACT ROUTER */}
-					{/* ADMIN */}
-					<Route path='/admin' component={Admin} />
-					<Route path='/login'
-						component={() => <LoginPage
-							id={this.state.id}
-							login={this.login}
-							logout={this.logout}
-						/>}
-					/>
-					{/* USER */}
-					{/* <Route path='/' /> */}
-				</div>
-			</Router>
-		)
-	}
+  render () {
+    return (
+      <Router>
+        <div>
+          <Button onClick={() => console.log(this.state)}>Log state</Button>
+          <Link to='/admin'>หน้าแอดมินจ้า</Link>
+          {/* TODO */}
+          {/* REACT ROUTER */}
+          {/* ADMIN */}
+          <Route path='/admin' component={Admin} />
+          <Route
+            path='/login'
+            component={() => (
+              <LoginPage
+                id={this.state.id}
+                login={this.login}
+                logout={this.logout}
+              />
+            )}
+          />
+          {/* USER */}
+          {/* <Route path='/' /> */}
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App
