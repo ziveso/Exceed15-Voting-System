f5c7f0385e749a57d6d407c2905106426a937124
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-07-09 23:33:59 +0700

    fix eslint

--- src/Login/User.js
+++ src/Login/User.js
@@ -1,6 +1,6 @@
 import studentIds from '../User/studentId'
 
-export function Login (studentId) {
+export const Login = studentId => {
   var currentStudent = null
   for (var i = 0; i < studentIds.length; i++) {
     if (studentId === studentIds[i]) {
@@ -17,13 +17,10 @@ export function Login (studentId) {
   return currentStudent
 }
 
-export function Logout () {
+export const Logout = () => {
   window.localStorage.studentId = null
 }
 
-export function isLogin () {
-  if (window.localStorage.studentId && window.localStorage.studentId.length === 10) {
-    return window.localStorage.studentId
-  }
-  return null
+export const isLogin = () => {
+  return (window.localStorage.studentId && window.localStorage.studentId.length === 10) || false
 }
