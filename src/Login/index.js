65d117f758ec4a0177f540bc3ca70b1c62a43a6b
winChawakorn <chawakorn.su@ku.th>
2018-07-10 01:05:58 +0700

    Remove login logout func from App

--- src/Login/index.js
+++ src/Login/index.js
@@ -24,24 +24,6 @@ class Login extends React.Component {
 
   render () {
     return (
-<<<<<<< HEAD
-      <div style={{ maring: 'auto', textAlign: 'center' }}>
-        <h1>EXCEED 15th</h1>
-        <div style={{
-          width: '400px',
-          height: '200px',
-          boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-          padding: '40px',
-          margin: 'auto'
-        }}>
-          <InputGroup style={{ width: '300px', height: '45px', marginBottom: '2em' }}>
-            <InputGroupAddon addonType='prepend'>
-              <InputGroupText>Student ID</InputGroupText>
-            </InputGroupAddon>
-            <Input placeholder='Input Student ID' value={this.state.id} onChange={this.handleChange} />
-          </InputGroup>
-          <Button style={{width: '80px'}}>Login</Button>
-=======
       <div style={full}>
         <div>
           <h1>EXCEED 15th</h1>
@@ -56,7 +38,6 @@ class Login extends React.Component {
             </InputGroup>
             <Button>Login</Button>
           </div>
->>>>>>> e10c9912e093531ca7322c06beb2285b9a13171a
         </div>
       </div>
     )
