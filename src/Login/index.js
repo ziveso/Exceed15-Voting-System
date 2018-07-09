877b237ed46d35b1e42f0fc007fab76406f7ef09
winChawakorn <chawakorn.su@ku.th>
2018-07-10 00:54:21 +0700

    Change size

--- src/Login/index.js
+++ src/Login/index.js
@@ -19,18 +19,18 @@ class Login extends React.Component {
         <h1>EXCEED 15th</h1>
         <div style={{
           width: '400px',
-          height: '400px',
+          height: '200px',
           boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
           padding: '40px',
           margin: 'auto'
         }}>
-          <InputGroup style={{ width: '300px', height: '45px', marginBottom: '10px' }}>
+          <InputGroup style={{ width: '300px', height: '45px', marginBottom: '2em' }}>
             <InputGroupAddon addonType='prepend'>
               <InputGroupText>Student ID</InputGroupText>
             </InputGroupAddon>
             <Input placeholder='Input Student ID' value={this.state.id} onChange={this.handleChange} />
           </InputGroup>
-          <Button>Login</Button>
+          <Button style={{width: '80px'}}>Login</Button>
         </div>
       </div >
     )
